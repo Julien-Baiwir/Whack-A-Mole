4657df1db7ae939864acfda96025fc00c51b768f
Julien-Baiwir <julien.freddy.baiwir@gmail.com>
2024-01-04 15:52:59 +0100

    gg

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,5 @@
+// ---- random background ---
+
 function PickNumber() {
     const randomNumber = Math.floor(Math.random() * 12) + 1;
     const selectedElement = document.getElementById(randomNumber.toString());
@@ -13,3 +15,15 @@ function PickNumber() {
 }
 
 setInterval(PickNumber, 1000);
+
+// -----ajoute un score ---
+
+let score = 0;
+
+document.body.addEventListener('click', function(event) {
+    const clickedElement = event.target;
+    if (clickedElement.classList.contains('grid-item') && clickedElement.classList.contains('red')) {
+        score++;
+        document.getElementById('scoreValue').textContent = ' ' + score;
+    }
+});
