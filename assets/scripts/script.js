7b51983504843bdf87ffd79077124c45001cf70a
Julien-Baiwir <julien.freddy.baiwir@gmail.com>
2024-01-04 16:38:52 +0100

    fonctionnel

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,28 +1,24 @@
-// ---- random background ---
-
 function PickNumber() {
     const randomNumber = Math.floor(Math.random() * 12) + 1;
     const selectedElement = document.getElementById(randomNumber.toString());
     
-    const previousSelected = document.querySelector('.grid-item.red');
+    const previousSelected = document.querySelector('.grid-item.selected');
     if (previousSelected) {
-        previousSelected.classList.remove('red');
+        previousSelected.classList.remove('selected');
     }
     
     if (selectedElement) {
-        selectedElement.classList.add('red');
+        selectedElement.classList.add('selected');
     }
 }
-
 setInterval(PickNumber, 1000);
 
-// -----ajoute un score ---
 
 let score = 0;
 
 document.body.addEventListener('click', function(event) {
     const clickedElement = event.target;
-    if (clickedElement.classList.contains('grid-item') && clickedElement.classList.contains('red')) {
+    if (clickedElement.classList.contains('grid-item') && clickedElement.classList.contains('selected')) {
         score++;
         document.getElementById('scoreValue').textContent = ' ' + score;
     }
